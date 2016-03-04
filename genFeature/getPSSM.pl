1d7e38762a1215404ebed67d967b3190f5b6d198
simochen <chenx324@yeah.net>
2016-03-05 00:08:11 +0800

    generate features

---
+++ genFeature/getPSSM.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use File::Basename;
+
+$pssmfile = $ARGV[0];
+
+@suffixlist = qw(.pssm .blast .fasta .dom);
+$dir = dirname($pssmfile);
+$base = basename($pssmfile,@suffixlist);
+
+$output = (join "/", $dir, $base).'_pssm.txt';
+print $output,"\n";
+
+open(pssm, "<$pssmfile");
+@lines = <pssm>;
+close(pssm);
+
+open(out, ">$output");
+
+shift(@lines);
+shift(@lines);
+shift(@lines);
+
+$line = shift(@lines);
+chomp($line);
+while ($line){	
+	$data = substr($line, 10, 59);
+	print out "$data\n";
+	$line = shift(@lines);
+	chomp($line);
+}
+
+close(out);
+
