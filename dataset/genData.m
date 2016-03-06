64174e48e8526fe49f868bd7c28e7861a558b631
simochen <chenx324@yeah.net>
2016-03-06 16:46:00 +0800

    generate data

---
+++ dataset/genData.m
@@ -0,0 +1,26 @@
+fid = fopen('multi354_target.txt');
+i = 0;
+while ~feof(fid)
+    i = i+1;
+    multi_y{i} =  fgetl(fid)';
+end
+fclose(fid);
+
+
+fid = fopen('single963_target.txt');
+i = 0;
+while ~feof(fid)
+    i = i+1;
+    single_y{i} =  fgetl(fid)';
+end
+fclose(fid);
+
+for i = 1:354
+    fname = sprintf('multi/%d.txt',i);
+    multi_X{i} = load(fname);
+end
+
+for i = 1:963
+    fname = sprintf('single/%d.txt',i);
+    single_X{i} = load(fname);
+end
