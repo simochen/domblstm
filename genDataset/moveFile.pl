1fd9cdf3475da5485d9e936675c1ef752549939a
simochen <chenx324@yeah.net>
2016-03-22 23:29:02 +0800

    moveFile

--- genDataset/moveFile.pl
+++ genDataset/moveFile.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/perl
 
+$combdir = "/home/chenxiao/domainDS/cullpdb/mul_dis_seg";
+
 $multiList = "/home/chenxiao/domainDS/m_multi.list";
 $discList = "/home/chenxiao/domainDS/m_disc.list";
 
-$multidir = "/home/chenxiao/domainDS/cullpdb/multi";
-$discdir = "/home/chenxiao/domainDS/cullpdb/disc";
+$multidir = "/home/chenxiao/domainDS/cullpdb/multi_sa";
+$discdir = "/home/chenxiao/domainDS/cullpdb/disc_sa";
 
 open(mlist, "<$multiList");
 @mlist = <mlist>;
@@ -14,33 +16,32 @@ open(dlist, "<$discList");
 @dlist = <dlist>;
 close(dlist);
 
-`rm -r $discdir`;
-`mkdir $discdir`;
-
 $cnt = 0;
 while($line = shift(@dlist)){
 	chomp($line);
 	$cnt++;
-	$orifasta = (join "/", $multidir, $line).".fasta";
-	$oripssm = (join "/", $multidir, $line).".pssm";
-	$tarfasta = (join "/", $discdir, $cnt).".fasta";
-	$tarpssm = (join "/", $discdir, $cnt).".pssm";
-	`mv $orifasta $tarfasta`;
-	if($line <= 500){
-		`mv $oripssm $tarpssm`;
-	}
+	$oriss = (join "/", $combdir, $line)."_ss.txt";
+	$oriacc = (join "/", $combdir, $line)."_acc.txt";
+	$oricob = (join "/", $combdir, $line)."_ssacc.txt";
+	$tarss = (join "/", $discdir, $cnt)."_ss.txt";
+	$taracc = (join "/", $discdir, $cnt)."_acc.txt";
+	$tarcob = (join "/", $discdir, $cnt)."_ssacc.txt";
+	`mv $oriss $tarss`;
+	`mv $oriacc $taracc`;
+	`mv $oricob $tarcob`;
 }
 
 $cnt = 0;
 while($line = shift(@mlist)){
 	chomp($line);
 	$cnt++;
-	$orifasta = (join "/", $multidir, $line).".fasta";
-	$oripssm = (join "/", $multidir, $line).".pssm";
-	$tarfasta = (join "/", $multidir, $cnt).".fasta";
-	$tarpssm = (join "/", $multidir, $cnt).".pssm";
-	`mv $orifasta $tarfasta`;
-	if($line <= 500){
-		`mv $oripssm $tarpssm`;
-	}
+	$oriss = (join "/", $combdir, $line)."_ss.txt";
+	$oriacc = (join "/", $combdir, $line)."_acc.txt";
+	$oricob = (join "/", $combdir, $line)."_ssacc.txt";
+	$tarss = (join "/", $multidir, $cnt)."_ss.txt";
+	$taracc = (join "/", $multidir, $cnt)."_acc.txt";
+	$tarcob = (join "/", $multidir, $cnt)."_ssacc.txt";
+	`mv $oriss $tarss`;
+	`mv $oriacc $taracc`;
+	`mv $oricob $tarcob`;
 }
